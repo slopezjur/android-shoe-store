a331edf14421e65491f6f6159cbdf815dc8c49aa
Sergio López <slopezjur@users.noreply.github.com>
2022-08-18 18:07:38 +0200

    add fragment ktx to allow activityViewModels

--- app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeListBinding
 
-    private lateinit var viewModel: ShoeListViewModel
+    private val viewModel: ShoeListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +24,6 @@ class ShoeListFragment : Fragment() {
     ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
-        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
 
         setHasOptionsMenu(true)
 
